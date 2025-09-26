5fdf7e50d7a6cd1f0492fa06ab190a01287d892c
Daniel <82724282+dotdani@users.noreply.github.com>
2025-09-26 18:13:19 -0500

    feat: agrega opción de configuración para habilitar/inhabilitar registro de usuarios

--- resources/js/pages/admin/site/edit.tsx
+++ resources/js/pages/admin/site/edit.tsx
@@ -2,8 +2,8 @@ import AdminSiteSettingsForm from '@/components/content/admin-site-settings-form
 import AdminLayout from '@/layouts/admin/admin-layout';
 import AppLayout from '@/layouts/app-layout';
 import { AppContentLayout } from '@/layouts/app/app-content-layout';
-import type { BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import type { BreadcrumbItem, SiteSettings } from '@/types';
+import { Head, usePage } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
 
 /**
@@ -12,6 +12,9 @@ import { useTranslation } from 'react-i18next';
 export default function UsersEdit() {
     // Obtiene las traducciones de la página.
     const { t } = useTranslation();
+
+    // Captura la configuración del sitio proporcionado por Inertia.
+    const { site_settings } = usePage<{ site_settings: SiteSettings }>().props;
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
@@ -25,7 +28,7 @@ export default function UsersEdit() {
             <Head title={t('siteSettings')} />
             <AdminLayout>
                 <AppContentLayout noMargin={true} fullWidth={true}>
-                    <AdminSiteSettingsForm />
+                    <AdminSiteSettingsForm settings={site_settings} />
                 </AppContentLayout>
             </AdminLayout>
         </AppLayout>
---
+++ resources/js/types/modules/site-settings.ts
@@ -0,0 +1,5 @@
+export interface SiteSettings {
+    is_user_registration_enabled: boolean;
+    created_at: string;
+    updated_at: string;
+}
