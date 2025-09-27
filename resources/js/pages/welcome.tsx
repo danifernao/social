49fedd796bf08ac24d4da294a26d8b94efa6aa98
danifernao <82724282+danifernao@users.noreply.github.com>
2025-09-27 16:17:02 -0500

    chore: elimina console.log en página de bienvenida

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -10,8 +10,6 @@ export default function Welcome() {
     // Captura la configuración del sitio proporcionado por Inertia.
     const { siteSettings } = usePage<{ siteSettings: SiteSettings }>().props;
 
-    console.log(siteSettings);
-
     return (
         <div className="bg-background flex min-h-screen flex-col items-center justify-center px-6 text-center">
             <Head title={t('welcome')} />
