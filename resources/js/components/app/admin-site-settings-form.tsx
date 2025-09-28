0bd8857c5eb85b830d9daba34b7568f53ed0378e
danifernao <82724282+danifernao@users.noreply.github.com>
2025-09-28 12:09:24 -0500

    refactor: renombra pass_confirmation a privileged_password en formularios y validaciones

--- resources/js/components/app/admin-site-settings-form.tsx
+++ resources/js/components/app/admin-site-settings-form.tsx
@@ -68,8 +68,8 @@ export default function AdminSiteSettingsForm({ settings }: AdminSiteSettingsFor
             <ConfirmActionDialog
                 open={isDialogOpen}
                 onOpenChange={closeDialog}
-                password={form.data.pass_confirmation}
-                onPasswordChange={(value) => form.setData('pass_confirmation', value)}
+                password={form.data.privileged_password}
+                onPasswordChange={(value) => form.setData('privileged_password', value)}
                 onConfirm={confirmAction}
             />
         </form>
