383164980a73e6ccdaa5175e88022006098461a6
danifernao <82724282+danifernao@users.noreply.github.com>
2025-10-11 18:04:57 -0500

    feat: añadir soporte para tipos de página informativa

--- resources/js/types/modules/page.ts
+++ resources/js/types/modules/page.ts
@@ -1,11 +1,15 @@
 export interface Page {
     id: number;
+    language: string;
+    type: PageType;
     title: string;
     slug: string;
-    language: string;
+    
     content: string | null;
     created_at: string;
 }
+
+export type PageType = 'policy' | 'guidelines' | 'normal';
 
 export interface Pages {
     data: Page[];
