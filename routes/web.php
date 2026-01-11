2f4a2b8f14f1aac1b35a56b75c78abdc45200f94
danifernao <82724282+danifernao@users.noreply.github.com>
2026-01-11 18:10:18 -0500

    fix: corrige conflicto de rutas en settings

--- routes/web.php
+++ routes/web.php
@@ -192,24 +192,24 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->group(function () {
             Route::redirect('settings', 'settings/profile');
 
-            Route::get('settings/profile', [SettingsProfileController::class, 'edit'])
+            Route::get('profile', [SettingsProfileController::class, 'edit'])
                 ->name('profile.edit');
-            Route::patch('settings/profile', [SettingsProfileController::class, 'update'])
+            Route::patch('profile', [SettingsProfileController::class, 'update'])
                 ->name('profile.update');
-            Route::delete('settings/profile', [SettingsProfileController::class, 'destroy'])
+            Route::delete('profile', [SettingsProfileController::class, 'destroy'])
                 ->name('profile.destroy');
 
-            Route::get('settings/password', [SettingsPasswordController::class, 'edit'])
+            Route::get('password', [SettingsPasswordController::class, 'edit'])
                 ->name('password.edit');
-            Route::put('settings/password', [SettingsPasswordController::class, 'update'])
+            Route::put('password', [SettingsPasswordController::class, 'update'])
                 ->name('password.update');
 
-            Route::get('settings/language', [SettingsLanguageController::class, 'edit'])
+            Route::get('language', [SettingsLanguageController::class, 'edit'])
                 ->name('language.edit');
-            Route::patch('settings/language', [SettingsLanguageController::class, 'update'])
+            Route::patch('language', [SettingsLanguageController::class, 'update'])
                 ->name('language.update');
 
-            Route::get('settings/appearance', function () {
+            Route::get('appearance', function () {
                 return Inertia::render('settings/appearance');
             })->name('appearance');
         });
