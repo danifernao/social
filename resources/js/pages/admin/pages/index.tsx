f4b8bce1623d85b5f679646289d52b0386082f53
danifernao <82724282+danifernao@users.noreply.github.com>
2026-01-24 15:22:50 -0500

    refactor: renombra admin-layout.tsx a layout.tsx

--- resources/js/pages/admin/pages/index.tsx
+++ resources/js/pages/admin/pages/index.tsx
@@ -1,7 +1,7 @@
 import AdminPageList from '@/components/app/admin-page-list';
 import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import AdminLayout from '@/layouts/app/admin/admin-layout';
+import AdminLayout from '@/layouts/app/admin/layout';
 import AppLayout from '@/layouts/kit/app-layout';
 import { AppContentLayout } from '@/layouts/kit/app/app-content-layout';
 import type { BreadcrumbItem } from '@/types';
