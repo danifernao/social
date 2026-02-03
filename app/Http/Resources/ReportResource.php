c4553ec1a474e5c8ffe9522a545ec28cb663b605
danifernao <82724282+danifernao@users.noreply.github.com>
2026-02-03 10:58:13 -0500

    refactor: normaliza reporter y resolver usando UserResource

--- app/Http/Resources/ReportResource.php
+++ app/Http/Resources/ReportResource.php
@@ -20,32 +20,27 @@ class ReportResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $reporter = $this->whenLoaded('reporter', function () {
+            return new UserResource($this->reporter);
+        });
+
+        $resolver = $this->whenLoaded('resolver', function () {
+            return new UserResource($this->resolver);
+        });
+
         return [
-            'id' => $this->id,
-
-            'reporter' => $this->reporter
-                ? [
-                    'id' => $this->reporter->id,
-                    'username' => $this->reporter->username,
-                ]
-                : null,
-            
-            'resolver' => $this->resolver
-                ? [
-                    'id' => $this->resolver->id,
-                    'username' => $this->resolver->username,
-                ]
-                : null,
-
-            'reportable_type' => $this->reportable_type,
-            'reportable_id' => $this->reportable_id,
-            'reportable_exists' => (bool) $this->reportable,
+            'id'                  => $this->id,
+            'reporter'            => $reporter,
+            'resolver'            => $resolver,
+            'reportable_type'     => $this->reportable_type,
+            'reportable_id'       => $this->reportable_id,
+            'reportable_exists'   => (bool) $this->reportable,
             'reportable_snapshot' => $this->reportable_snapshot,
-            'reporter_note' => $this->reporter_note,
-            'resolver_note' => $this->resolver_note,
-            'closed_at' => $this->closed_at,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'reporter_note'       => $this->reporter_note,
+            'resolver_note'       => $this->resolver_note,
+            'closed_at'           => $this->closed_at,
+            'created_at'          => $this->created_at,
+            'updated_at'          => $this->updated_at,
         ];
     }
 }
