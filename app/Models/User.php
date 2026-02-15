2a2574fc9d3ea9f31928a9c92ee606e18ffa121b
danifernao <82724282+danifernao@users.noreply.github.com>
2026-02-15 10:27:21 -0500

    refactor: usa Storage para generar la URL del avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * Modelo que representa a un usuario registrado.
@@ -79,7 +80,7 @@ class User extends Authenticatable implements MustVerifyEmail
     public function getAvatarUrlAttribute(): ?string
     {
         return $this->avatar_path
-            ? asset('storage/' . $this->avatar_path)
+            ? Storage::disk('public')->url($this->avatar_path)
             : null;
     }
 
